a2a76d09616c895ed47414e82a79a9583014fb77
greymd <greengregson@gmail.com>
2019-03-03 23:59:39 +0000

    update

--- gengo2019.sh
+++ gengo2019.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-## nohup bash gengo2019.sh >> $HOME/gengo2019.log &
+
+## Post tweets
+## $ nohup bash gengo2019.sh >> $HOME/gengo2019.log &
+
+## Delete tweets
+## $ cat gengo2019.log  | grep tweet_id | awk '{print "t delete status -f "$NF"; sleep 1"}'  | bash
 
 # run script like
 readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")" && pwd)"
@@ -8,7 +13,7 @@ _res=
 while read -r id gengo; do
   echo "[$id] $gengo is start" >&2
   ## Post command
-  _post="@tos 次の元号は $gengo （ネタバレ）"
+  _post="@tos 次は $gengo か・・・（ネタバレ）"
   _res=$(t update "${_post}")
   tweet_id=$(echo "${_res}" | tail -n 1 | tr -d '`' | awk '{print $5}')
   if ! [[ "$tweet_id" =~ ^[0-9]*$  ]];then
