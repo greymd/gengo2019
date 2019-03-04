9384ec2d53a20b1a926fee686f163a9e978f3acd
greymd <greengregson@gmail.com>
2019-03-04 00:26:21 +0000

    update error message

--- gengo2019.sh
+++ gengo2019.sh
@@ -18,13 +18,14 @@ while read -r id gengo; do
   echo "[$id] $gengo is start" >&2
   ## Post command
   _post="@tos 新${ZWS1}し${ZWS2}い${ZWS3}元${ZWS1}号${ZWS2}は${ZWS3}「$gengo」${ZWS1}か${ZWS2}・${ZWS3}・${ZWS1}・${ZWS2}（${ZWS3}ネ${ZWS1}タ${ZWS2}バ${ZWS3}レ${ZWS1}）"
+  _status="$?"
   _res=$(tweet.sh post "${_post}")
   tweet_id=$(echo "${_res}" | jq .id)
-  if ! [[ "$tweet_id" =~ ^[0-9]*$  ]];then
-    echo "[$id] $gengo is skipped"
+  if [[ ${_status} -ne 0 ]] || ! [[ "$tweet_id" =~ ^[0-9]*$  ]];then
+    echo "[$id][$gengo] is skipped"
   else
-    echo "[$id] $gengo is posted"
-    echo "[$id] tweet_id $tweet_id"
+    echo "[$id][$gengo] is posted"
+    echo "[$id][$gengo] tweet_id $tweet_id"
   fi
   sleep 3
 done < "${THIS_DIR}"/shuf_filtered4
