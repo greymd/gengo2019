c4f0119033330fddf452bf815b496caaf2091be0
greymd <greengregson@gmail.com>
2019-03-04 00:24:07 +0000

    update

--- gengo2019.sh
+++ gengo2019.sh
@@ -6,16 +6,20 @@
 ## Delete tweets
 ## $ cat gengo2019.log  | grep tweet_id | awk '{print "t delete status -f "$NF"; sleep 1"}'  | bash
 
-# run script like
+ZWS1=$'\u200b'
+ZWS2=$'\u200c'
+ZWS3=$'\u200d'
+# ZWS4=$'\ufeff'
+
 readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")" && pwd)"
 _post=
 _res=
 while read -r id gengo; do
   echo "[$id] $gengo is start" >&2
   ## Post command
-  _post="@tos 次は $gengo か・・・（ネタバレ）"
-  _res=$(t update "${_post}")
-  tweet_id=$(echo "${_res}" | tail -n 1 | tr -d '`' | awk '{print $5}')
+  _post="@tos 新${ZWS1}し${ZWS2}い${ZWS3}元${ZWS1}号${ZWS2}は${ZWS3}「$gengo」${ZWS1}か${ZWS2}・${ZWS3}・${ZWS1}・${ZWS2}（${ZWS3}ネ${ZWS1}タ${ZWS2}バ${ZWS3}レ${ZWS1}）"
+  _res=$(tweet.sh post "${_post}")
+  tweet_id=$(echo "${_res}" | jq .id)
   if ! [[ "$tweet_id" =~ ^[0-9]*$  ]];then
     echo "[$id] $gengo is skipped"
   else
